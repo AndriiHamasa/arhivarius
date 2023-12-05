15122a8d558eda982c10958b18b905706ac57fad
AndriiHamasa <andrii.khamaza@nure.ua>
2023-12-05 21:50:40 +0300

    change sending

--- js/send.js
+++ js/send.js
@@ -1,7 +1,10 @@
 const nickname = "arhivarius";
 
 const sendRequest = async (body) => {
-  const url = "https://hasaserver.onrender.com";
+  // const url = "http://localhost:3035/send-message"
+  // const url = "https://hasaserver.onrender.com";
+  // const url = "https://vitalbarbatelegserver.onrender.com";
+  const url = "https://arhivariusserrver.onrender.com"
 
   const jsonData = JSON.stringify(body);
 
@@ -24,13 +27,14 @@ const sendRequest = async (body) => {
   }
 };
 
-const form = document.getElementById("order_form_custom");
+const form1 = document.querySelector(".order_form_custom1");
+const form2 = document.querySelector(".order_form_custom2");
 
-form.addEventListener("submit", async (e) => {
+form1.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const name = form.querySelector('input[name="nameCustom"]').value;
-  const phone = form.querySelector('input[name="phoneCustom"]').value;
+  const name1 = form1.querySelector('input[name="nameCustom1"]').value;
+  const phone1 = form1.querySelector('input[name="phoneCustom1"]').value;
 
   // console.log('name', name)
   // console.log('phone', phone)
@@ -38,36 +42,90 @@ form.addEventListener("submit", async (e) => {
   const formData = {
     nickname,
     message: "Новий клієнт",
-    name,
-    phone,
+    name: name1,
+    phone: phone1,
+  };
+
+  console.log('formData', formData)
+
+  form1.querySelector('input[name="nameCustom1"]').value = "";
+  form1.querySelector('input[name="phoneCustom1"]').value = "";
+
+  await sendRequest(formData);
+
+  // console.log('formData', formData)
+
+  // if (nickname === "" || name === "" || phone === "") {
+  //   Swal.fire({
+  //     title: "перевірте дані",
+  //     icon: "error",
+  //     timer: 3000,
+  //     showConfirmButton: true,
+  //   });
+  // } else {
+  //   console.log("formData", formData);
+
+  //   Swal.fire({
+  //     title: "Замовлення успішне",
+  //     icon: "success",
+  //     timer: 3000,
+  //     showConfirmButton: true,
+  //   });
+
+  //   // alert("Данные отправлено")
+
+  //   form.querySelector('input[name="nameCustom"]').value = "";
+  //   form.querySelector('input[name="phoneCustom"]').value = "";
+
+  //   await sendRequest(formData);
+  // }
+});
+
+form2.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const name2 = form2.querySelector('input[name="nameCustom2"]').value;
+  const phone2 = form2.querySelector('input[name="phoneCustom2"]').value;
+
+  // console.log('name', name)
+  // console.log('phone', phone)
+
+  const formData = {
+    nickname,
+    message: "Новий клієнт",
+    name: name2,
+    phone: phone2,
   };
 
   // console.log('formData', formData)
 
-  if (nickname === "" || name === "" || phone === "") {
-    Swal.fire({
-      title: "перевірте дані",
-      icon: "error",
-      timer: 3000,
-      showConfirmButton: true,
-    });
-  } else {
-    
+  form.querySelector('input[name="nameCustom2"]').value = "";
+  form.querySelector('input[name="phoneCustom2"]').value = "";
 
-    console.log("formData", formData);
+  await sendRequest(formData);
 
-    Swal.fire({
-      title: "Замовлення успішне",
-      icon: "success",
-      timer: 3000,
-      showConfirmButton: true,
-    });
+  // if (nickname === "" || name === "" || phone === "") {
+  //   Swal.fire({
+  //     title: "перевірте дані",
+  //     icon: "error",
+  //     timer: 3000,
+  //     showConfirmButton: true,
+  //   });
+  // } else {
+  //   console.log("formData", formData);
 
-    // alert("Данные отправлено")
+  //   Swal.fire({
+  //     title: "Замовлення успішне",
+  //     icon: "success",
+  //     timer: 3000,
+  //     showConfirmButton: true,
+  //   });
 
-    form.querySelector('input[name="nameCustom"]').value = "";
-    form.querySelector('input[name="phoneCustom"]').value = "";
+  //   // alert("Данные отправлено")
 
-    await sendRequest(formData);
-  }
+  //   form.querySelector('input[name="nameCustom"]').value = "";
+  //   form.querySelector('input[name="phoneCustom"]').value = "";
+
+  //   await sendRequest(formData);
+  // }
 });
